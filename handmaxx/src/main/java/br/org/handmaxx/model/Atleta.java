ce7b56cf936a952b6103d042a145c0128f572e09
Matheus Naves <mnaves.dev@gmail.com>
2024-09-10 20:02:30 -0300

    atleta getters and setters + diagrama

--- handmaxx/src/main/java/br/org/handmaxx/model/Atleta.java
+++ handmaxx/src/main/java/br/org/handmaxx/model/Atleta.java
@@ -17,5 +17,44 @@ public class Atleta extends DefaultEntity {
 
     private Categoria categoria;
 
-    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
 }
