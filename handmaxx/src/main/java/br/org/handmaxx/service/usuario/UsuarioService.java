2e45f174ff15f313b017df369bc35a76147d08d5
danielmariae <lucasdrodrigues70@gmail.com>
2024-09-21 21:02:15 -0300

    creating and updating exceptions

--- handmaxx/src/main/java/br/org/handmaxx/service/usuario/UsuarioService.java
+++ handmaxx/src/main/java/br/org/handmaxx/service/usuario/UsuarioService.java
@@ -5,5 +5,9 @@ import br.org.handmaxx.dto.usuario.UsuarioResponseDTO;
 
 public interface UsuarioService {
     public UsuarioResponseDTO findByLoginAndSenha(String login, String senha);
-    public UsuarioResponseDTO create(UsuarioDTO dto);
+    public UsuarioResponseDTO findById(Long id);
+    public UsuarioResponseDTO create(UsuarioDTO dto);    
+    public UsuarioResponseDTO update(UsuarioDTO dto, Long id);    
+    public void delete(Long id);    
+
 }
--- handmaxx/src/main/java/br/org/handmaxx/service/usuario/UsuarioServiceImpl.java
+++ handmaxx/src/main/java/br/org/handmaxx/service/usuario/UsuarioServiceImpl.java
@@ -5,9 +5,13 @@ import br.org.handmaxx.dto.usuario.UsuarioDTO;
 import br.org.handmaxx.dto.usuario.UsuarioResponseDTO;
 import br.org.handmaxx.repository.UsuarioRepository;
 import br.org.handmaxx.service.usuario.hash.HashService;
+import br.org.handmaxx.app.error.custom.CustomException;
+import br.org.handmaxx.app.error.global.ErrorResponse;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.PersistenceException;
 import jakarta.validation.Valid;
+
 
 @ApplicationScoped
 public class UsuarioServiceImpl implements UsuarioService {
@@ -24,6 +28,15 @@ public class UsuarioServiceImpl implements UsuarioService {
     }
 
     @Override
+    public UsuarioResponseDTO findById(Long id){
+        Usuario user = usuarioRepository.findById(id);
+        if(user == null){
+            throw new CustomException(new ErrorResponse("Usuário não encontrado", "UsuarioServiceImpl(update)", 404));
+        }
+        return UsuarioResponseDTO.valueOf(user);
+    }
+
+    @Override
     public UsuarioResponseDTO create(@Valid UsuarioDTO dto){
         Usuario user = new Usuario();
 
@@ -31,8 +44,45 @@ public class UsuarioServiceImpl implements UsuarioService {
         user.setLogin(dto.login());
         user.setSenha(hashService.getHashSenha(dto.senha()));
         
-        return null;
+        try {
+            usuarioRepository.persist(user);
+        } catch (PersistenceException e) {
+            ErrorResponse errorResponse = new ErrorResponse(
+                "Erro ao criar usuário", 
+                "UsuarioServiceImpl(insert): " + e.getMessage(),
+                500);
+                
+            throw new CustomException(errorResponse);
+        }
+        return UsuarioResponseDTO.valueOf(user);
     }
 
-    
+    @Override
+    public UsuarioResponseDTO update(UsuarioDTO dto, Long id){
+        Usuario user = usuarioRepository.findById(id);
+        if(user == null){
+            throw new CustomException(new ErrorResponse("Usuário não encontrado", "UsuarioServiceImpl(update)", 404));
+        }
+        user.setCnpj(dto.cnpj());
+        user.setLogin(dto.login());
+        if(!dto.senha().isEmpty()){
+            user.setSenha(hashService.getHashSenha(dto.senha()));
+        }
+
+        return UsuarioResponseDTO.valueOf(user);
+    }
+
+    @Override
+    public void delete(Long id){
+        Usuario user = usuarioRepository.findById(id);
+        if(user == null){
+            throw new CustomException(new ErrorResponse("Usuário não encontrado", "UsuarioServiceImpl(delete)", 404));
+        }
+        try {
+            usuarioRepository.delete(user);
+        } catch (Exception e) {
+            throw new CustomException(new ErrorResponse("Erro no servidor.", "UsuarioServiceImpl(delete)", 500));
+        }
+    }
+
 }
