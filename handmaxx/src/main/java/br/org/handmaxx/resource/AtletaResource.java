4db9d11d67eb3d2509db9602899ff24f148375ca
Matheus Naves <mnaves.dev@gmail.com>
2024-09-23 12:15:51 -0300

    crud de atleta por usuario logado

--- handmaxx/src/main/java/br/org/handmaxx/resource/AtletaResource.java
+++ handmaxx/src/main/java/br/org/handmaxx/resource/AtletaResource.java
@@ -2,9 +2,13 @@ package br.org.handmaxx.resource;
 
 import java.util.List;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
 import br.org.handmaxx.dto.atleta.AtletaDTO;
 import br.org.handmaxx.dto.atleta.AtletaResponseDTO;
 import br.org.handmaxx.service.atleta.AtletaService;
+import io.quarkus.security.Authenticated;
+
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -16,21 +20,27 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 
+@Authenticated
 @Path("/atletas")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AtletaResource {
-    
+
+    @Inject
+    JsonWebToken jwt;
+
     @Inject
     AtletaService atletaService;
 
     @POST
     @Transactional
     public Response create(@Valid AtletaDTO dto) {
+
         return Response.status(201).entity(atletaService.create(dto)).build();
     }
 
@@ -38,6 +48,7 @@ public class AtletaResource {
     @Path("/{id}")
     @Transactional
     public Response update(@Valid AtletaDTO dto, @PathParam("id") Long id) {
+
         atletaService.update(dto, id);
         return Response.status(Status.NO_CONTENT).build();
     }
@@ -46,21 +57,26 @@ public class AtletaResource {
     @Path("/{id}")
     @Transactional
     public Response delete(@PathParam("id") Long id) {
+
         atletaService.delete(id);
         return Response.status(Status.NO_CONTENT).build();
     }
 
+    // Permitir que qualquer usuário autenticado possa buscar um atleta por ID
     @GET
     @Path("/{id}")
     @Transactional
+
     public Response getById(@PathParam("id") Long id) {
+
         return Response.status(200).entity(atletaService.findById(id)).build();
     }
 
-    //Novo endpoint para buscar por nome
+    // Permitir que qualquer usuário autenticado possa buscar atletas por nome
     @GET
     @Path("/nome/{nome}")
     @Transactional
+
     public Response getByNome(@PathParam("nome") String nome) {
         List<AtletaResponseDTO> atletas = atletaService.findByNome(nome);
         return Response.ok(atletas).build();
